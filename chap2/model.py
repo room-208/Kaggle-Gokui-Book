4dbf069ae907add193f8bb4ef7d805e9c1d513a0
room-208 <yumalegomindstorm@gmail.com>
2023-07-05 13:10:46 +0000

    ch2-2を読みました

--- chap2/model.py
+++ chap2/model.py
@@ -5,7 +5,7 @@ def create_model():
     model = nn.Sequential(
         nn.Linear(64, 32),
         nn.ReLU(),
-        nn.Linear(32, 10),
+        nn.Linear(32, 16),
         nn.ReLU(),
         nn.Linear(16, 10),
     )
