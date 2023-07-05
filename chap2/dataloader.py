98611c218f1bb669017077c82e3949b265635fd2
room-208 <yumalegomindstorm@gmail.com>
2023-07-05 14:18:54 +0000

    valに名前を修正しました

--- chap2/dataloader.py
+++ chap2/dataloader.py
@@ -23,21 +23,21 @@ def create_dataloder_ver2():
     X_train, X_test, y_train, y_test = train_test_split(
         X, y, test_size=0.3, stratify=y, random_state=0
     )
-    X_train, X_valid, y_train, y_valid = train_test_split(
+    X_train, X_val, y_train, y_val = train_test_split(
         X_train, y_train, test_size=0.3, stratify=y_train, random_state=0
     )
 
     X_train = torch.tensor(X_train, dtype=torch.float32)
-    X_valid = torch.tensor(X_valid, dtype=torch.float32)
+    X_val = torch.tensor(X_val, dtype=torch.float32)
     X_test = torch.tensor(X_test, dtype=torch.float32)
     y_train = torch.tensor(y_train, dtype=torch.int64)
-    y_valid = torch.tensor(y_valid, dtype=torch.int64)
+    y_val = torch.tensor(y_val, dtype=torch.int64)
     y_test = torch.tensor(y_test, dtype=torch.int64)
 
     train_dataset = TensorDataset(X_train, y_train)
-    valid_dataset = TensorDataset(X_valid, y_valid)
+    val_dataset = TensorDataset(X_val, y_val)
     test_dataset = TensorDataset(X_test, y_test)
     train_loader = DataLoader(train_dataset, batch_size=64, shuffle=False)
-    valid_loader = DataLoader(valid_dataset, batch_size=64, shuffle=False)
+    val_loader = DataLoader(val_dataset, batch_size=64, shuffle=False)
     test_loader = DataLoader(test_dataset, batch_size=64, shuffle=False)
-    return train_loader, valid_loader, test_loader
+    return train_loader, val_loader, test_loader
