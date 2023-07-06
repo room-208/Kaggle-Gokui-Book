1e32e9484d7cabdde7c56bc92c63f2de0a1bcc72
room-208 <yumalegomindstorm@gmail.com>
2023-07-06 14:03:46 +0000

    train, validateを作成しました

---
+++ chap3/train.py
@@ -0,0 +1,37 @@
+import torch
+from tqdm import tqdm
+
+from validate import validate_1epoch
+
+
+def train_1epoch(model, train_loader, lossfn, optimizer, device):
+    model.train()
+    total_loss = 0.0
+    total_acc = 0.0
+
+    for x, y in tqdm(train_loader, total=len(train_loader)):
+        x = x.to(device)
+        y = y.to(device)
+        optimizer.zero_grad()
+        out = model(x)
+        loss = lossfn(out, y)
+        loss.backward()
+        optimizer.step()
+        total_loss += float(loss) * x.size(0)
+        _, pred = torch.max(out, dim=1)
+        total_acc += torch.sum(pred == y)
+
+    avg_loss = total_loss / len(train_loader.dataset)
+    avg_acc = total_acc / len(train_loader.dataset)
+    return avg_acc, avg_loss
+
+
+def train(model, lossfn, optimizer, train_loader, val_loader, n_epochs, device):
+    for epoch in tqdm(range(n_epochs)):
+        train_acc, train_loss = train_1epoch(
+            model, train_loader, lossfn, optimizer, device
+        )
+        val_acc, val_loss = validate_1epoch(model, val_loader, lossfn, device)
+        print(f"epoch = {epoch}")
+        print(f"train_acc = {train_acc}, train_loss = {train_loss}")
+        print(f"val_acc = {val_acc}, val_loss = {val_loss}")
---
+++ chap3/validate.py
@@ -0,0 +1,22 @@
+import torch
+from tqdm import tqdm
+
+
+def validate_1epoch(model, val_loader, lossfn, device):
+    model.eval()
+    total_loss = 0.0
+    total_acc = 0.0
+
+    with torch.no_grad():
+        for x, y in tqdm(val_loader):
+            x = x.to(device)
+            y = y.to(device)
+            out = model(x)
+            loss = lossfn(out, y)
+            _, pred = torch.max(out, 1)
+            total_loss += float(loss) * x.size(0)
+            total_acc += torch.sum(pred == y)
+
+    avg_loss = total_loss / len(val_loader.dataset)
+    avg_acc = total_acc / len(val_loader.dataset)
+    return avg_acc, avg_loss
