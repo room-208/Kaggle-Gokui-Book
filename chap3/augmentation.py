f43c1596eee4ddb0ff16a13cd23030baea622520
room-208 <yumalegomindstorm@gmail.com>
2023-07-07 15:57:31 +0000

    augmentationを作成しました

---
+++ chap3/augmentation.py
@@ -0,0 +1,23 @@
+from torchvision import transforms
+
+
+def setup_center_crop_transform():
+    return transforms.Compose(
+        [
+            transforms.Resize(256),
+            transforms.CenterCrop(224),
+            transforms.ToTensor(),
+            transforms.Normalize([0.485, 0.456, 0.406], [0.299, 0.224, 0.225]),
+        ]
+    )
+
+
+def setup_crop_flip_transform():
+    return transforms.Compose(
+        [
+            transforms.RandomResizedCrop(224),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            transforms.Normalize([0.485, 0.456, 0.406], [0.299, 0.224, 0.225]),
+        ]
+    )
