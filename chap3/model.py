c25007ff089eeaf7f255416b641ed577eaeb613f
room-208 <yumalegomindstorm@gmail.com>
2023-07-07 18:06:50 +0000

    warningを反映しました

--- chap3/model.py
+++ chap3/model.py
@@ -1,9 +1,9 @@
-import torchvision
+from torchvision.models import resnet50, ResNet50_Weights
 from torch import nn
 
 
 def setup_model(device):
-    model = torchvision.models.resnet50(pretrained=True)
+    model = resnet50(weights=ResNet50_Weights.IMAGENET1K_V2)
     model.fc = nn.Linear(model.fc.in_features, 2)
     model = model.to(device)
     return model
