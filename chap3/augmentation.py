aaa4acbf403b0d2db4b319eb708f8e088a8432df
room-208 <yumalegomindstorm@gmail.com>
2023-07-07 17:21:30 +0000

    crop_flipを戻します

--- chap3/augmentation.py
+++ chap3/augmentation.py
@@ -15,9 +15,7 @@ def setup_center_crop_transform():
 def setup_crop_flip_transform():
     return transforms.Compose(
         [
-            # transforms.RandomResizedCrop(224),
-            transforms.Resize(256),
-            transforms.CenterCrop(224),
+            transforms.RandomResizedCrop(224),
             transforms.RandomHorizontalFlip(),
             transforms.ToTensor(),
             transforms.Normalize([0.485, 0.456, 0.406], [0.299, 0.224, 0.225]),
