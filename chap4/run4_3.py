4a00a94d964b99d997919b9a978af9d7f4b2f844
room-208 <yumalegomindstorm@gmail.com>
2023-08-11 01:16:15 +0000

    utilsなどに移動させました

--- chap4/run4_3.py
+++ chap4/run4_3.py
@@ -1,71 +1,12 @@
 import argparse
 
 import numpy as np
-import torch
-from cirtorch.datasets.genericdataset import ImagesFromList
 from cirtorch.datasets.testdataset import configdataset
 from cirtorch.utils.evaluate import compute_map_and_print
 from device import setup_device
 from model import setup_model
-from torch.utils.data import DataLoader
-from torchvision import transforms
-from tqdm import tqdm
 from transform import setup_transform
-
-
-def extract_vectors(
-    data_dir,
-    model,
-    image_files,
-    input_size,
-    out_dim,
-    transform,
-    device,
-    bbxs=None,
-):
-    dataloader = DataLoader(
-        ImagesFromList(
-            root=data_dir,
-            images=image_files,
-            imsize=input_size,
-            transform=transform,
-            bbxs=bbxs,
-        ),
-        batch_size=1,
-        shuffle=False,
-        num_workers=2,
-        pin_memory=True,
-    )
-
-    with torch.no_grad():
-        vecs = torch.zeros(out_dim, len(image_files))
-        for i, x in tqdm(enumerate(dataloader), total=len(dataloader)):
-            x = x.to(device)
-            embed = model(x).squeeze()
-            vecs[:, i] = embed
-            # print(embed.shape)
-
-    return vecs
-
-
-def get_query_index_images(cfg, dryrun):
-    index_images = [cfg["im_fname"](cfg, i) for i in range(cfg["n"])]
-    query_images = [cfg["qim_fname"](cfg, i) for i in range(cfg["nq"])]
-    bbxs = [tuple(cfg["gnd"][i]["bbx"]) for i in range(cfg["nq"])]
-    # try:
-    #    bbxs = [tuple(cfg["gnd"][i]["bbx"]) for i in range(cfg["nq"])]
-    # except KeyError:
-    #    bbxs = None
-
-    if dryrun:
-        index_images = index_images[:10]
-        query_images = query_images[:10]
-        bbxs = bbxs[:10]
-
-    print("len(index_images)=", len(index_images))
-    print("len(query_images)=", len(query_images))
-    print("len(bbxs)=", len(bbxs))
-    return index_images, query_images, bbxs
+from utils import extract_vectors, get_query_index_images
 
 
 def run():
